1fe2122154680e5dc4698ed54f003aa15e5c1337
bm prime <103988519+bm-prime@users.noreply.github.com>
2024-07-16 15:46:41 +0300

    Guided project - Calculate final GPA

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,39 +1,47 @@
-﻿// Guided project - Calculate and print student grades
+﻿// Guided project - Calculate final GPA
 
-// initialize variables - graded assignments 
-int currentAssignments = 5;
+string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
 
-int sophia1 = 93;
-int sophia2 = 87;
-int sophia3 = 98;
-int sophia4 = 95;
-int sophia5 = 100;
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
 
-int nicolas1 = 80;
-int nicolas2 = 83;
-int nicolas3 = 82;
-int nicolas4 = 88;
-int nicolas5 = 85;
+int gradeA = 4;
+int gradeB = 3;
 
-int zahirah1 = 84;
-int zahirah2 = 96;
-int zahirah3 = 73;
-int zahirah4 = 85;
-int zahirah5 = 79;
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
 
-int jeong1 = 90;
-int jeong2 = 92;
-int jeong3 = 98;
-int jeong4 = 100;
-int jeong5 = 97;
+int totalCreditHour = course1Credit + course2Credit + course3Credit + course4Credit + course5Credit;
+int totalGradePoints = course1Credit * course1Grade;
+totalGradePoints += course2Credit * course2Grade;
+totalGradePoints += course3Credit * course3Grade;
+totalGradePoints += course4Credit * course4Grade;
+totalGradePoints += course5Credit * course5Grade;
+decimal GPA = (decimal)totalGradePoints / totalCreditHour;
+int leadingDigit = totalGradePoints / totalCreditHour;
+int firstDigit = (int)(GPA * 10) % 10;
+int secondDigit = (int)(GPA * 100) % 10;
+string finalGPA = $"{leadingDigit}.{firstDigit}{secondDigit}";
 
-decimal averageSophia = ((decimal)sophia1 + sophia2 + sophia3 + sophia4 + sophia5) / currentAssignments;
-decimal averageNicolas = ((decimal)nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5) / currentAssignments;
-decimal averageZahira = ((decimal)zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5) / currentAssignments;
-decimal averageJeong = ((decimal)jeong1 + jeong2 + jeong3 + jeong4 + jeong5) / currentAssignments;
+Console.WriteLine($@"Student:  {studentName}
 
-Console.WriteLine($@"Student     Grade
-Sophia      {averageSophia} A
-Nicolas     {averageNicolas} B
-Zahirah     {averageZahira} B
-Jeong       {averageJeong} A");
+Course              Grade    Credit Hours
+{course1Name}           {course1Grade}      {course1Credit}
+{course2Name}           {course2Grade}      {course2Credit}
+{course3Name}           {course3Grade}      {course3Credit}
+{course4Name}    {course4Grade}      {course4Credit}
+{course5Name}        {course5Grade}      {course5Credit}
+
+Final GPA:            {finalGPA}");
+
