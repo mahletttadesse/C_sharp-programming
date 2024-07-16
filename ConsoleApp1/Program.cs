a95adb6da4f3ae7f3b2305b7d028af758796b165
bm prime <103988519+bm-prime@users.noreply.github.com>
2024-07-16 14:22:03 +0300

    Perform basic operations on numbers in C#

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,45 +1,19 @@
-﻿// Perform basic string formatting in C#
-
-Console.WriteLine("Hello\tWorld!"); // tab
-Console.WriteLine("Hello\nWorld!"); // new line
-Console.WriteLine("Hello \"World\"!"); // unescape
-
-// Putting it all together
-Console.WriteLine("\nGenerating invoices for customer \"Contoso Corp\" ... \n");
-Console.WriteLine("Invoice: 1021\t\tComplete!");
-Console.WriteLine("Invoice: 1022\t\tComplete!");
-Console.WriteLine("\nOutput Directory:\t");
-Console.Write(@"c:\invoices"); // verbatim string litral
-Console.Write('\n'); // new line as a char
-
-// To generate Japanese invoices:
-// Nihon no seikyū-sho o seisei suru ni wa:
-Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
-// User command to run an application
-Console.WriteLine(@"c:\invoices\app.exe -j");
-
-// string concatenation 
+﻿// Perform basic operations on numbers in C#
 
 string firstName = "Bob";
-string message = "Hello " + firstName; // no space and !
-Console.Write('\n');
-Console.WriteLine(message);
-string greeting = "Hello ";
-Console.WriteLine(greeting + " " + firstName + "!"); // with space and !, inside console
+int widgetsSold = 7;
+Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets."); // Mix data types to force implicit type conversions
+Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets."); // Mix data types to force implicit type conversions
 
-// Combine strings using string interpolation
+int first = 7;
+int second = 5;
+decimal quotient = (decimal)first / (decimal)second; // casting decimal on int
+Console.WriteLine(quotient);
 
-message = $"{greeting} {firstName}!"; // interpolation
-Console.WriteLine(message);
+Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}"); // modulus and interpoltion
 
-string projectName = "First-Project";
-Console.WriteLine($@"C:\Output\{projectName}\Data");
-Console.Write('\n');
-// Use character escape sequences, verbatim strings, unicode, and string interpolation to generate the output.
-
-projectName = "ACME";
-string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
-Console.WriteLine($@"View English output:
-    c:\Exercise\{projectName}\data.txt" + '\n');
-Console.WriteLine($@"{russianMessage}:
-    c:\Exercise\{projectName}\ru-RU\data.txt");
+int fahrenheit = 94;
+Console.Write("The temperature is ");
+Console.Write(((decimal)fahrenheit - 32) * 5 / 9);
+Console.Write(" Celsius.");
