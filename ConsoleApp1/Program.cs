7e71fdaa523e3d07aacb06695ff1317d6e9306f1
mahletttadesse <175498883+mahletttadesse@users.noreply.github.com>
2024-07-16 20:11:20 +0300

    Store and iterate through sequences of data using Arrays and the foreach statement in C#

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,31 +1,42 @@
-﻿// decision-making logic in C#
+﻿// Store and iterate through sequences of data using Arrays and the foreach statement in C# 
 
-Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+/*
+string[] fraudulentOrderIDs = new string[3];
 
-// Your code goes here
+fraudulentOrderIDs[0] = "A123";
+fraudulentOrderIDs[1] = "B456";
+fraudulentOrderIDs[2] = "C789";
+// fraudulentOrderIDs[3] = "D000";
+*/
 
-if (daysUntilExpiration == 0)
+/*string[] fraudulentOrderIDs = { "A123", "B456", "C789" };
+
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+
+Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders to process.");
+
+int[] inventory = { 200, 450, 700, 175, 250 };
+int sum = 0;
+int bin = 0;
+foreach (int items in inventory)
 {
-    Console.WriteLine("Your subscription has expired.");
+    sum += items;
+    bin++;
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
 }
+Console.WriteLine($"We have {sum} items in inventory.");*/
 
-else if (daysUntilExpiration == 1)
+string[] fraudulentOrderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+foreach (string items in fraudulentOrderIDs)
 {
-    Console.WriteLine("Your subscription expires within a day!");
-    discountPercentage = 20;
-    Console.WriteLine($"Renew now and save {discountPercentage}%!");
+    if (items.StartsWith('B'))
+    {
+        Console.WriteLine(items);
+    }
 }
-
-else if (daysUntilExpiration <= 5)
-{
-    discountPercentage = 10;
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. \nRenew now and save {discountPercentage}%!");
-}
-
-else if (daysUntilExpiration <= 10)
-{
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
-
