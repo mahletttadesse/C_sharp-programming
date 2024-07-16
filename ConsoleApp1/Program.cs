232be0a05b52c591b71c97fdf47b045e53b16f94
bm prime <103988519+bm-prime@users.noreply.github.com>
2024-07-16 11:17:45 +0300

    Structuring the console Project using gitignore to make it acceptable standard.
    Starting a project of learning C# and .NET in ConsoleApp1
    
    learning C# on Microsoft: First code on Hello World

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿
+//Console.WriteLine("Hello, World!");
+
+Console.Write("Congratulations! ");
+Console.WriteLine("You wrote your first lines of code. ");
+Console.WriteLine("Congratulations!");
+Console.WriteLine("You wrote your first lines of code.");
+Console.WriteLine("Did you get the difference between the two?");
