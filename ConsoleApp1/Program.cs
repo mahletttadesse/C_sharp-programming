6c97ce769cfe53be1c24e1e69c55a989c139b1d6
bm prime <103988519+bm-prime@users.noreply.github.com>
2024-07-16 12:20:57 +0300

    Exercise - Print literal values

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,10 @@
 ﻿
 //Console.WriteLine("Hello, World!");
 
-Console.WriteLine("This is the first line.");
-Console.WriteLine("This is the second line.");
+Console.WriteLine('b'); // Printing char in C#
+Console.WriteLine(123); // Printing int litrals in C#
+Console.WriteLine(0.25F); // Printing float litrals in C#
+Console.WriteLine(2.625); // Printing double litrals in C#
+Console.WriteLine(12.39816m); // Printing decimal litrals in C#
+Console.WriteLine(true); // Printing bool litrals in C#
+
